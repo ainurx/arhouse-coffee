04f8e85d6f4cfea22c29d1c3d4518a840a461fd6
ainurx <ainurrahmat1@gmail.com>
2023-08-29 23:44:38 +0800

    [master] Lil bit footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import { Container, Typography } from '@mui/material'
 import React from 'react'
 
 const Footer = () => {
@@ -8,11 +8,11 @@ const Footer = () => {
         <img src={require('../assets/banner3.png')} 
           style={{ width: '100%' }}/>
         <div
+          className='pd-24'
           style={{
             position: 'absolute',
             right: '24px',
             bottom: -15,
-            padding: '18px',
             backgroundColor: '#013020'
           }}
         >
@@ -20,12 +20,50 @@ const Footer = () => {
             sx={{ color: 'white'}}>Jl. Pelita Raya 23, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90231</Typography>
           <Typography variant='body2' 
             sx={{ color: 'white' }}>Buka setiap hari 10.00 - 22.00</Typography>
-          <hr/>
+          <hr className='my-8'/>
           <Typography variant='body2'
             sx={{ color: 'white' }}>Kontak Kami : +6287237287121</Typography>
         </div>
       </div>
 
+      <div className='main-footer'>
+        <Container maxWidth='xl'>
+          <div className='pd-48-40 d-flex justify-spc-between'>
+            <div>
+              <img src={require('../assets/arhouse-colored-logo.png')} />
+              <Typography 
+                variant='h6' 
+                className='font-primary font-bold'
+              >Arhouse Coffee, join us</Typography>
+              <Typography
+                variant='body2'
+                className='font-secondary'
+              >Copyright ⓒ 2020 - 2021 First Boulevard, all rights reserved.</Typography>
+            </div>
+
+            <div className='d-flex justify-spc-around'>
+              <div>
+                <Typography 
+                  variant='h6'
+                  className='font-primary font-bold'
+                >TENTANG KAMI</Typography>
+              </div>
+              <div>
+                <Typography 
+                  variant='h6'
+                  className='font-primary font-bold'
+                >MENU</Typography>
+              </div>
+              <div>
+                <Typography 
+                  variant='h6'
+                  className='font-primary font-bold'
+                >MEDIA</Typography>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
